3f6376385afd0d1d03733d57fe42c4baca86cf9b
treythomas123 <treythomas123@gmail.com>
2015-03-29 21:13:16 -0600

    Add solution to problem 6

---
+++ 06/solution.swift
@@ -0,0 +1,25 @@
+// The sum of the squares of the first ten natural numbers is,
+// 1² + 2² + ... + 10² = 385
+//
+// The square of the sum of the first ten natural numbers is,
+// (1 + 2 + ... + 10)² = 55² = 3025
+//
+// Hence the difference between the sum of the squares of the first ten natural
+// numbers and the square of the sum is 3025 − 385 = 2640.
+//
+// Find the difference between the sum of the squares of the first one hundred
+// natural numbers and the square of the sum.
+
+var sumOfSquares = 0
+var sum = 0
+
+for i in 1...100 {
+    sumOfSquares += i * i
+    sum += i
+}
+
+let squareOfSums = sum * sum
+
+let difference = squareOfSums - sumOfSquares
+
+println(difference)
